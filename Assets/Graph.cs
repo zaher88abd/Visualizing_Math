d3fd589d8a7dd16ffa00e2bbade2a7f2fe53cbe5
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-05-21 22:19:35 -0300

    Setting the Parent

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -11,18 +11,20 @@ public class Graph : MonoBehaviour
 
     void Awake()
     {
-        Vector3 scale = Vector3.one / 5f;
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
         Vector3 position;
         //position.y = 0f;
         position.z = 0f;
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < resolution; i++)
         {
             Transform point = Instantiate(pointPrefab);
             //			point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
-            position.x = (i + 0.5f) / 5f - 1f;
+            position.x = (i + 0.5f)* step - 1f;
             position.y = position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform,false);
         }
     }
 
