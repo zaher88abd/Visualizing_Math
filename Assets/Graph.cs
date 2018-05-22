a15567757c9aba108b3ac77af3223b6b31f5de3a
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-05-21 21:48:56 -0300

    Code the loop

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Graph : MonoBehaviour {
+public class Graph : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform pointPrefab;
+
+    public void Awake()
+    {
+        int i = 0;
+        while (i < 10)
+        {
+            //			i = i + 1;
+            Transform point = Instantiate(pointPrefab);
+            point.localPosition = Vector3.right * i;
+            i = i + 1;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
